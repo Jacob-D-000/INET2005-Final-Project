6b072460c682d6c5ed7e2ce1bb0a706e941a13ed
Jacob <jacobdimoff@gmail.com>
2023-12-02 11:24:41 -0400

    added primary and unique modifers to data fields in the mirgration files

--- finalproject-app/database/migrations/2023_12_02_133828_create_items_table.php
+++ finalproject-app/database/migrations/2023_12_02_133828_create_items_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('items', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->foreignIdFor(Category::class);
-            $table->string("title");
+            $table->string("title")->unique();
             $table->string("description");
             $table->decimal("price");
             $table->bigInteger("quantity");
