d21f4ac6eac197effef6c52a7e1a6dba8b33af58
Jacob <jacobdimoff@gmail.com>
2023-12-02 23:31:53 -0400

    Reforamtted the file a little bit

--- finalproject-app/resources/views/categories/create.blade.php
+++ finalproject-app/resources/views/categories/create.blade.php
@@ -16,12 +16,12 @@
     <h1>Create a Category</h1>
 </header>
 <body>
-    <form action="{{route('categories.store')}}" method="post">
+    <form action="{{ route('categories.store') }}" method="post">
         @csrf
         <label for="name">Category Name: </label>
-        <input type="text" name="name" id="name" value="{{old('name')}}" required>
+        <input type="text" name="name" id="name" value="{{ old('name') }}" required>
         @error("name")
-        <p>{{$message}}</p>
+        <p>{{ $message }}</p>
         @enderror
         <button type="submit">Create a new Category</button>
     </form>    
