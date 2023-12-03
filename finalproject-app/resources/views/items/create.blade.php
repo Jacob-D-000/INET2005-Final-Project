e14598432f253aa0db0b30abe175b38ead4d2aad
Jacob <jacobdimoff@gmail.com>
2023-12-02 22:41:53 -0400

    added a create page to be servered

---
+++ finalproject-app/resources/views/items/create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<!-- 
+	Project:
+	Author: Jacob Dimoff
+	Date:
+	Filename:
+	Purpose:
+	-->
+	
+<head>
+	<title>Category</title>
+</head>
+<header>
+    <h1>Category</h1>
+</header>
+<body>
+</body>
+</html>
