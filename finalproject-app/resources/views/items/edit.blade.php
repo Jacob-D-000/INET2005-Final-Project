f202e6eeb545a3907fd276a5114e2e58a783218e
Jacob <jacobdimoff@gmail.com>
2023-12-03 09:41:47 -0400

    create and index and edit page

---
+++ finalproject-app/resources/views/items/edit.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<!-- 
+	Project:
+	Author: Jacob Dimoff
+	Date:
+	Filename:
+	Purpose:
+	-->
+	
+<head>
+	<title>Edit and Item</title>
+</head>
+<header>
+    <h1>Edit and Item</h1>
+</header>
+<body>
+</body>
+</html>
---
+++ finalproject-app/resources/views/items/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<!-- 
+	Project:
+	Author: Jacob Dimoff
+	Date:
+	Filename:
+	Purpose:
+	-->
+	
+<head>
+	<title>Item</title>
+</head>
+<header>
+    <h1>Item</h1>
+</header>
+<body>
+</body>
+</html>
